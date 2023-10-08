781689613250e3e4753829f1b898a7afbbbbb20f
chenshitian <546329059@qq.com>
2023-10-08 10:14:51 +0800

    fea: apple 1

--- vuepress-starter/deploy.sh
+++ vuepress-starter/deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zuifengshaonian/zuifengshaonian.github.io.git master:gh-pages
+git push -f git@github.com:zuifengshaonian/blogs.git master:gh-pages
 
 cd -
