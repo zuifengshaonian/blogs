f9d372ca05d9f044c719dfc5eddd43e72e559229
chenshitian <546329059@qq.com>
2023-10-08 10:08:30 +0800

    fea: config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/blogs/",
 
   lang: "zh-CN",
   title: "博客演示",
